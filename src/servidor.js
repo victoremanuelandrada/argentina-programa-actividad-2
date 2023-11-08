134982cc71eec0a71359e1c5bab02ce1604c2044
victoremanuelandrada <lvpemanuelandrada@gmail.com>
2023-11-08 19:49:23 -0300

    Agrego sybida de archivos

--- src/servidor.js
+++ src/servidor.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const bodyParse = require('body-parser')
+const bodyParse = require('body-parser');
+const fileUpload = require('express-fileupload');
+
 const usuarioRouter = require('./routers/UsuarioRouters.js');
 const AutenticacionRoutes = require('./routers/autenticacionRoutes.js');
+const ArchivosRouter = require('./routers/ArchivosRouter.js');
 
 const app = express();
 const PORT = 3000;
@@ -12,10 +15,14 @@ const PORT = 3000;
 /*app.get('/', (req, res) => {
   res.send('Hola Mundo!');
 })*/
-
+//MiddleWare
 app.use(bodyParse.json());
+app.use(fileUpload());
+
+//Rutas
 app.use(usuarioRouter);
 app.use(AutenticacionRoutes);
+app.use(ArchivosRouter);
 
 app.listen(PORT, () => {
   console.log(`servidor iniciado en el puerto: ${PORT}`);
